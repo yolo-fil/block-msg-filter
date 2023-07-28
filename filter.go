9455e15ec0628b7b9240ddaef4fe92f189b5b411
yolo-fil <yolo-fil.gh@sign-up.email>
2023-07-28 19:12:34 +0200

    test

--- filter.go
+++ filter.go
@@ -18,43 +18,34 @@ var timestmp = time.Unix(0, 0)
 var cfg cfgFormat
 
 func Filter[T any](slice []T, predicate func(T, cfgFormat) bool) []T {
-
 	log.Infow("yolo-fil filtering")
-
 	file, err := os.Stat(os.Getenv("YOLO_FIL_CONFIG_PATH"))
-
 	if err != nil {
-		// fmt.Println(err)
+		log.Errorf("yolo-fil: error stat'ing file")
+		return slice
 	}
-
 	modifiedtime := file.ModTime()
-
 	if modifiedtime.After(timestmp) {
 		log.Infow("yolo-fil filtering: loading config file")
-
 		content, err := ioutil.ReadFile(os.Getenv("YOLO_FIL_CONFIG_PATH"))
-
 		if err != nil {
-			// log.Fatal("Error when opening file: ", err)
+			log.Errorf("yolo-fil: error opening file")
+			return slice
 		}
-
 		cfg := cfgFormat{}
 		err = json.Unmarshal(content, &cfg)
 		if err != nil {
-			log.Fatal("Error during Unmarshal(): ", err)
+			log.Errorf("yolo-fil: error unmarshalling file")
+			return slice
 		}
-
 		timestmp = modifiedtime
 	}
-
 	result := make([]T, 0, len(slice))
-
 	for _, element := range slice {
-		if predicate(element) {
+		if predicate(element, cfg) {
 			result = append(result, element)
 		}
 	}
-
 	return result
 }
 
