99bcf1e8da3456592c3460ac4d387130ad08c137
yolo-fil <yolo-fil.gh@sign-up.email>
2023-07-28 20:42:19 +0200

    test

--- filter.go
+++ filter.go
@@ -10,14 +10,16 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
-type cfgFormat map[string][]uint64
-
 var log = logging.Logger("yolo-fil")
 var timestmp = time.Unix(0, 0)
-var cfg cfgFormat
+var cfg map[string][]uint64
+
+func init() {
+	log.Warnf("yolo-fil: active")
+}
 
 func FilterMsgList(msgs []*types.SignedMessage) []*types.SignedMessage {
-	log.Infow("yolo-fil filtering")
+	log.Infow("yolo-fil: filter msg list")
 	file, err := os.Stat(os.Getenv("YOLO_FIL_CONFIG_PATH"))
 	if err != nil {
 		log.Errorf("yolo-fil: error stat'ing file")
@@ -25,13 +27,13 @@ func FilterMsgList(msgs []*types.SignedMessage) []*types.SignedMessage {
 	}
 	modifiedtime := file.ModTime()
 	if modifiedtime.After(timestmp) {
-		log.Infow("yolo-fil filtering: loading config file")
+		log.Infow("yolo-fil: loading config file")
 		content, err := ioutil.ReadFile(os.Getenv("YOLO_FIL_CONFIG_PATH"))
 		if err != nil {
 			log.Errorf("yolo-fil: error opening file")
 			return msgs
 		}
-		cfg := cfgFormat{}
+		cfg := map[string][]uint64{}
 		err = json.Unmarshal(content, &cfg)
 		if err != nil {
 			log.Errorf("yolo-fil: error unmarshalling file")
@@ -40,27 +42,29 @@ func FilterMsgList(msgs []*types.SignedMessage) []*types.SignedMessage {
 		timestmp = modifiedtime
 	}
 	result := make([]*types.SignedMessage, 0, len(msgs))
-	for _, element := range msgs {
-		if val, ok := cfg[element.Message.To.String()]; ok {
+	log.Infow("yolo-fil: cfg loaded")
+	for _, msg := range msgs {
+		log.Infow("yolo-fil: check msg", "cid", msg.Message.Cid().String())
+		if val, ok := cfg[msg.Message.To.String()]; ok {
+			log.Infow("yolo-fil: filter msg", "cid", msg.Message.Cid().String(), "to", msg.Message.To.String())
 			if val[0] == 0 {
+				log.Warnw("yolo-fil: filter applied", "cid", msg.Message.Cid().String(), "to", msg.Message.To.String(), "method", 0)
 				break
 			} else {
+				okm := true
 				for _, m := range val {
-					if uint64(element.Message.Method) == m {
-						break
-					} else {
-						result = append(result, element)
+					if ok && uint64(msg.Message.Method) == m {
+						log.Warnw("yolo-fil: filter applied", "cid", msg.Message.Cid().String(), "to", msg.Message.To.String(), "method", m)
+						okm = false
 					}
+				}
+				if okm {
+					result = append(result, msg)
 				}
 			}
 		} else {
-			result = append(result, element)
+			result = append(result, msg)
 		}
 	}
 	return result
-}
-
-func DefaultFilter(msg *types.SignedMessage, cfg cfgFormat) bool {
-	log.Infow("yolo-fil DefaultFilter Cid:", msg.Message.Cid())
-	return true
 }
